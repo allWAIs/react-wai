f5e4060b39bcb8a794b4b9d66152c8c05cd7e42e
chjy0202 <chjy0202@naver.com>
2022-11-18 20:43:01 +0900

    feat: Alert 컴포넌트
    - 사이즈 지정 가능
    - 불러오고 삭제하는 시점은 사용자 조작

---
+++ src/components/Alert/Alert.styled.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const StyledAlert = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  border-radius: 4px;
+  background: hsl(200deg 20% 90%);
+
+  &:empty {
+    display: none;
+  }
+`;
+
+export function Alert({ width, height, children }) {
+  return (
+    <>
+      <StyledAlert role="alert" style={{ width, height }}>
+        {children}
+      </StyledAlert>
+    </>
+  );
+}
