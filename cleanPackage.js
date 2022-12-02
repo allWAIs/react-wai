751354467b2a37bdcb1db5cd4d7064ba0f831358
codevil <namhundred@naver.com>
2022-12-02 20:57:35 +0900

    config: clean Packagejson
    package.json 파일 정리

---
+++ cleanPackage.js
@@ -0,0 +1,10 @@
+import { readFileSync, writeFileSync } from 'fs';
+
+const primitiveText = readFileSync('package.json');
+
+const objectText = JSON.parse(primitiveText);
+delete objectText.devDependencies;
+delete objectText.dependencies;
+delete objectText.peerDependencies;
+delete objectText.scripts;
+writeFileSync('./dist/package.json', JSON.stringify(objectText, null, 2));
