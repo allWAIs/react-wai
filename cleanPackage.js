c76e3372e516c65af5d9f18ec08da584119b433c
codevil <namhundred@naver.com>
2022-12-06 20:49:45 +0900

    config: amend dependencies
    package.json
    react, react-dom, styled-component를
    peerdependencies에서 dependencies로 이동

--- cleanPackage.js
+++ cleanPackage.js
@@ -5,7 +5,6 @@ const primitiveText = readFileSync('package.json');
 const objectText = JSON.parse(primitiveText);
 
 delete objectText.devDependencies;
-delete objectText.dependencies;
 delete objectText.scripts;
 
 writeFileSync('./dist/package.json', JSON.stringify(objectText, null, 2));
