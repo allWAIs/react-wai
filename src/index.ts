b8ee287804d5ea50f57f9ec4eed1dcca4f6bcb12
codevil <namhundred@naver.com>
2022-12-18 18:34:58 +0900

    feat: add export Switch
    Switch export 추가

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import { A11yHidden } from './A11yHidden';
 import {Breadcrumb} from "./Breadcrumb"
 import { Title } from './Title';
 import { Link } from './Link';
-export { Alert, A11yHidden, Title, Link, Breadcrumb };
+import { Switch } from './Switch';
+export { Alert, A11yHidden, Title, Link, Breadcrumb, Switch };
