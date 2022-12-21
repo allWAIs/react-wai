a0e80586666536c94187a9ce976b2756d6c2d983
codevil <namhundred@naver.com>
2022-12-21 15:54:40 +0900

    feat: add Alert, Breadcrumb props
    Alert, Breadcrumb에 background, color를 변경할 수 있는 props 추가

--- src/Alert/Alert.tsx
+++ src/Alert/Alert.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { DetailedHTMLProps } from 'react';
 import styled from '@emotion/styled';
 
-export interface AlertProps {
+export interface AlertProps
+  extends DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
   width?: string;
   height?: string;
+  color?: string;
+  bg?: string;
   children?: string;
 }
 
-const StyledAlert = styled.div`
+const StyledAlert = styled.div<AlertProps>`
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 10px;
   border-radius: 4px;
-  background: hsl(200deg 20% 90%);
-
   &:empty {
     display: none;
   }
 `;
 
-export function Alert({ width, height, children }: AlertProps): JSX.Element {
+export function Alert({
+  width,
+  height,
+  children,
+  color = 'black',
+  bg = 'hsl(200deg 20% 90%)',
+}: AlertProps): JSX.Element {
   return (
-    <StyledAlert role="alert" style={{ width, height }}>
+    <StyledAlert
+      role="alert"
+      style={{ width, height, color: color, background: bg }}
+    >
       {children}
     </StyledAlert>
   );
