e2fa5849a0ce4e24fdb2602f8d8551bb1db957c3
loco9939 <kls9939@naver.com>
2023-01-04 20:25:06 +0900

    refactor: Add restProps Type
    - restProps 타입 추가
    - 컴포넌트 이름 변경
    - storybook 설정 변경

---
+++ src/A/A.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import { KeyboardEvent, MouseEvent } from 'react';
+import styled from '@emotion/styled';
+
+interface AProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  > {
+  as?: 'img';
+  alt?: string;
+  src?: string;
+  href?: string;
+  children?: React.ReactNode;
+  restProps?: unknown[];
+}
+
+const StyledLinkSpan = styled.span`
+  :hover,
+  :focus,
+  :hover::before,
+  :focus::before {
+    color: #000;
+    cursor: pointer;
+    outline: 0.2em solid hsl(219deg 63% 44%);
+    outline-offset: 0.2em;
+  }
+  color: #009;
+  background: transparent;
+  text-decoration: underline;
+`;
+
+const StyledLinkImg = styled.img`
+  :hover,
+  :focus,
+  :hover::before,
+  :focus::before {
+    color: #000;
+    cursor: pointer;
+    outline: 0.2em solid hsl(219deg 63% 44%);
+    outline-offset: 0.2em;
+  }
+  color: #009;
+  background: transparent;
+  text-decoration: underline;
+`;
+
+export function A({ as, alt, src, children, href, ...restProps }: AProps) {
+  const handleLinkClick = (e: KeyboardEvent | MouseEvent, url: string) => {
+    if (
+      (e as unknown as KeyboardEvent)['key'] !== 'Enter' &&
+      e.type !== 'click'
+    ) {
+      return;
+    }
+
+    window.location.href = url;
+
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
+  return (
+    <>
+      {as === 'img' ? (
+        <StyledLinkImg
+          tabIndex={0}
+          role="link"
+          onClick={(e) => handleLinkClick(e, href as string)}
+          onKeyDown={(e) => handleLinkClick(e, href as string)}
+          src={src}
+          alt={alt}
+          {...restProps}
+        />
+      ) : (
+        <StyledLinkSpan
+          tabIndex={0}
+          role="link"
+          onClick={(e) => handleLinkClick(e, href as string)}
+          onKeyDown={(e) => handleLinkClick(e, href as string)}
+          {...restProps}
+        >
+          {children}
+        </StyledLinkSpan>
+      )}
+    </>
+  );
+}
+
+A.defaultProps = {
+  as: 'span',
+};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { Alert } from './Alert';
 import { A11yHidden } from './A11yHidden';
 import { Breadcrumb } from './Breadcrumb';
 import { Title } from './Title';
-import { A11yLink } from './A11yLink';
+import { A } from './A';
 import { Switch } from './Switch';
-export { Alert, A11yHidden, Title, A11yLink, Breadcrumb, Switch };
+export { Alert, A11yHidden, Title, A, Breadcrumb, Switch };
