1951fa99c3bc6748a3e53a1c06d4ae30eb15e844
Ellis <nardi0302@gmail.com>
2023-01-25 20:32:37 +0900

    fix: Remove unrelated files
    - List 컴포넌트와 관련없는 파일을 제외하였습니다

--- src/components/TabMenu/TabList.jsx
+++
@@ -1,8 +0,0 @@
-import { List } from '../List';
-import { useDirection } from './context';
-
-export function TabList(props) {
-  const direction = useDirection();
-
-  return <List role="tablist" direction={direction} {...props} />;
-}
--- src/components/TabMenu/context.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react';
-
-export const TabContext = createContext();
-TabContext.displayName = 'Tab';
-
-export const useDirection = () => {
-  const { direction } = useContext(TabContext);
-  return direction;
-};
