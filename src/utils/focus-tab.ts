54077ba8226952ca8ec5de97f9103707d61a7f66
Ellis <nardi0302@gmail.com>
2023-02-06 17:28:26 +0900

    feat: Add return types for util functions

--- src/utils/focus-tab.ts
+++ src/utils/focus-tab.ts
@@ -18,39 +18,27 @@ const focusableSelector = `
   .replace(/\n\s+/g, '')
   .trim();
 
-const tabbableSelector = focusableSelector.replace(
-  /\[tabindex\]/,
-  '[tabindex]:not([tabindex^="-"])'
-);
+const tabbableSelector = focusableSelector.replace(/\[tabindex\]/, '[tabindex]:not([tabindex^="-"])');
 
-export const isFocusableElement = (Element: HTMLElement) => {
+export const isFocusableElement = (Element: HTMLElement): boolean => {
   const $wrapper = document.createElement('div');
   $wrapper.append(Element);
   return Boolean($wrapper.querySelector(focusableSelector));
 };
 
-export const getFocusableChild = (Element: HTMLElement) =>
-  Element.querySelector(focusableSelector);
+export const getFocusableChild = (Element: HTMLElement): HTMLElement | null => Element.querySelector(focusableSelector);
 
-export const getFocusableChildren = (Element: HTMLElement) =>
-  Array.from(Element.querySelectorAll<HTMLElement>(focusableSelector));
+export const getFocusableChildren = (Element: HTMLElement): HTMLElement[] =>
+  Array.from(Element.querySelectorAll(focusableSelector));
 
-export const getTabbableChildren = (Element: HTMLElement) =>
-  Array.from(Element.querySelectorAll<HTMLElement>(tabbableSelector));
+export const getTabbableChildren = (Element: HTMLElement): HTMLElement[] =>
+  Array.from(Element.querySelectorAll(tabbableSelector));
 
-export const removeTabbable = (Element: HTMLElement) =>
-  Element.setAttribute('tabindex', '-1');
-export const restoreTabbable = (Element: HTMLElement) =>
-  Element.setAttribute('tabindex', '0');
-export const toggleTabbable = (
-  Element: HTMLElement,
-  force: boolean | undefined
-) => {
+export const removeTabbable = (Element: HTMLElement): void => Element.setAttribute('tabindex', '-1');
+export const restoreTabbable = (Element: HTMLElement): void => Element.setAttribute('tabindex', '0');
+export const toggleTabbable = (Element: HTMLElement, force: boolean | undefined): void => {
   if (force === undefined) {
-    Element.setAttribute(
-      'tabindex',
-      Element.getAttribute('tabindex') === '0' ? '-1' : '0'
-    );
+    Element.setAttribute('tabindex', Element.getAttribute('tabindex') === '0' ? '-1' : '0');
   } else {
     force ? restoreTabbable(Element) : removeTabbable(Element);
   }
