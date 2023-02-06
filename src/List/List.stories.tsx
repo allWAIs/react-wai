35d67cfbc841ecc61ee55cc0f14538027545e004
Ellis <nardi0302@gmail.com>
2023-02-06 16:32:48 +0900

    feat: Update storybook
    - List를 중첩하여 사용하는 경우를 테스트하기 위한 story를 추가하였습니다

--- src/List/List.stories.tsx
+++ src/List/List.stories.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { List, ListProps } from './List';
 import { ListItem } from './ListItem';
+import { Title } from '../Title';
 
 export default {
   title: 'List',
   component: List,
   args: {
     as: 'ul',
+    style: { listStyle: 'none', gap: '10px' },
     direction: 'row',
     children: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((key) => (
       <ListItem key={key}>
@@ -37,28 +39,6 @@ Default.args = {
   'data-id': '123',
 };
 
-export const RowList = Template.bind({});
-RowList.parameters = {
-  docs: {
-    description: {
-      story: '순서가 없는 리스트 - 가로 방향',
-    },
-  },
-};
-
-export const ColumnList = Template.bind({});
-ColumnList.args = {
-  as: 'ol',
-  direction: 'col',
-};
-ColumnList.parameters = {
-  docs: {
-    description: {
-      story: '순서가 있는 리스트 - 세로 방향',
-    },
-  },
-};
-
 export const StyledList = Template.bind({});
 StyledList.args = {
   direction: 'col',
@@ -69,6 +49,78 @@ StyledList.args = {
   )),
 };
 StyledList.parameters = {
+  docs: {
+    description: {
+      story: '스타일을 적용한 리스트 - 세로 방향',
+    },
+  },
+};
+
+const webtoonData = [
+  {
+    day: '월요웹툰',
+    webtoons: ['월요웹툰1', '월요웹툰2', '월요웹툰3'],
+  },
+  {
+    day: '화요웹툰',
+    webtoons: ['화요웹툰1', '화요웹툰2', '화요웹툰3'],
+  },
+  {
+    day: '수요웹툰',
+    webtoons: ['수요웹툰1', '수요웹툰2', '수요웹툰3'],
+  },
+  {
+    day: '목요웹툰',
+    webtoons: ['목요웹툰1', '목요웹툰2', '목요웹툰3'],
+  },
+  {
+    day: '금요웹툰',
+    webtoons: ['금요웹툰1', '금요웹툰2', '금요웹툰3'],
+  },
+  {
+    day: '토요웹툰',
+    webtoons: ['토요웹툰1', '토요웹툰2', '토요웹툰3'],
+  },
+  {
+    day: '일요웹툰',
+    webtoons: ['일요웹툰1', '일요웹툰2', '일요웹툰3'],
+  },
+];
+
+export const NestedList = Template.bind({});
+NestedList.args = {
+  direction: 'row',
+  children: webtoonData.map(({ day, webtoons }) => (
+    <ListItem key={day} style={{ textAlign: 'center', background: 'yellow' }}>
+      <Title style={{ width: '100%' }}>{day}</Title>
+      <List
+        key={day}
+        style={{
+          listStyle: 'none',
+          margin: '0 16px',
+          padding: '10px',
+          background: 'pink',
+          alignItems: 'center',
+          gap: '10px',
+        }}
+        direction="col"
+        nested
+      >
+        {webtoons.map((title) => (
+          <ListItem key={title} style={{ width: '100%', height: '100px', textAlign: 'center', background: 'coral' }}>
+            <Title lv={3} style={{ width: '100%' }}>
+              {title}
+            </Title>
+            <a href="#">링크1</a>
+            <a href="#">링크2</a>
+            <a href="#">링크3</a>
+          </ListItem>
+        ))}
+      </List>
+    </ListItem>
+  )),
+};
+NestedList.parameters = {
   docs: {
     description: {
       story: '스타일을 적용한 리스트 - 세로 방향',
