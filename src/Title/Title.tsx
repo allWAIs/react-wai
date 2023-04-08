62af85d96860c89e42a55ada5475bd670142973e
Ellis <nardi0302@gmail.com>
2023-04-08 18:56:50 +0900

    Fix: Type Error & focusable control
    - resetProps에 대한 타입을 올바르게 수정하였습니다
    - emotion으로 마이그레이션 후 사용하지 않는 prop을 제거하였습니다
    - focusable이 아닌 경우를 올바르게 처리하였습니다

--- src/Title/Title.tsx
+++ src/Title/Title.tsx
@@ -2,27 +2,16 @@ import React, { ElementType } from 'react';
 import styled from '@emotion/styled';
 import { A11yHidden } from '../A11yHidden';
 
-export interface TitleProps
-  extends React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLHeadingElement>,
-    HTMLHeadingElement
-  > {
+export interface TitleProps extends PropsWithHTMLAttr<HTMLHeadingElement> {
   lv?: 1 | 2 | 3 | 4 | 5 | 6 | '1' | '2' | '3' | '4' | '5' | '6';
   hidden?: boolean;
   focusable?: boolean;
-  children?: string;
-  restProps?: unknown[];
-  as?: (ElementType<any> & string) | undefined;
-  forwardedAs?: string | React.ComponentType<any>;
+  children: React.ReactNode;
+  as?: ElementType<any> & string;
+  [key: string]: unknown;
 }
 
-export function Title({
-  lv = 2,
-  hidden,
-  focusable,
-  children,
-  ...restProps
-}: TitleProps) {
+export function Title({ lv = 2, hidden, focusable, children, ...restProps }: TitleProps): JSX.Element {
   const componentName: `h${typeof lv}` = `h${lv}`;
 
   return hidden ? (
@@ -30,11 +19,7 @@ export function Title({
       {children}
     </A11yHidden>
   ) : (
-    <StyledHeading
-      as={componentName}
-      tabIndex={focusable ? 0 : -1}
-      {...restProps}
-    >
+    <StyledHeading as={componentName} tabIndex={focusable ? 0 : undefined} {...restProps}>
       {children}
     </StyledHeading>
   );
